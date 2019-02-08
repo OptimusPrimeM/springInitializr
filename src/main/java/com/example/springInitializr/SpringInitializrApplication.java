990d63663ae45e85867aca50c6571774c9b2ac4c
Menik <menikdisney@gmail.com>
2019-02-08 12:03:23 +0600

    Initial commit

---
+++ src/main/java/com/example/springInitializr/SpringInitializrApplication.java
@@ -0,0 +1,14 @@
+package com.example.springInitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInitializrApplication.class, args);
+	}
+
+}
+
